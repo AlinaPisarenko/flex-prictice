6f055a5028ed1a6f23547fccbae3366f03e9f278
Alina Pisarenko <pisarenko.alina@gmail.com>
2022-11-16 14:38:30 -0500

    initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='container'>
+    <div className='child' id='one'>1</div>
+    <div className='child' id='two'>2</div>
+    <div className='child' id='three'>3</div>
+    <div className='child' id='four'>4</div>
+    <div className='child' id='five'>5</div>
+  </div>
+
     </div>
   );
 }
